9dfe66771932a458d3f2c12ab350c91258435cb4
Ritesh Yadav <riteshyadav070802@gmail.com>
2023-06-20 19:36:21 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,jsonify
 import cloudpickle
 
-model = cloudpickle.load(open('model.pkl','rb'))
+model = cloudpickle.load(open('./model.pkl','rb'))
 
 app=Flask(__name__)
 
